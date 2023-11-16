1f1919f5876defa008b858702578da631d3792cd
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-11-16 23:27:48 +0100

    javadoc fix

--- common/src/main/java/de/florianmichael/rclasses/common/StringUtils.java
+++ common/src/main/java/de/florianmichael/rclasses/common/StringUtils.java
@@ -156,7 +156,7 @@ public final class StringUtils {
     }
 
     /**
-     * Normalizes the given enum name to a readable string (e.g. "TEST_ENUM" -> "Test enum"). This method is used to
+     * Normalizes the given enum name to a readable string (e.g. "TEST_ENUM" becomes "Test enum"). This method is used to
      * generate a readable name for the enum. "$" will be removed, "_" will be replaced with " ",
      * and the first letter will be uppercase.
      *
