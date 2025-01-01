7dcd1a6738e6eb4b4b335823e219b8e01959dddb
FlorianMichael <florian.michael07@gmail.com>
2025-01-01 01:07:56 +0100

    Auto: 2025

--- math/src/main/java/de/florianmichael/rclasses/math/geometry/Trigonometry.java
+++ math/src/main/java/de/florianmichael/rclasses/math/geometry/Trigonometry.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of RClasses - https://github.com/FlorianMichael/RClasses
- * Copyright (C) 2023-2024 FlorianMichael/EnZaXD <florian.michael07@gmail.com> and contributors
+ * Copyright (C) 2023-2025 FlorianMichael/EnZaXD <florian.michael07@gmail.com> and contributors
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
